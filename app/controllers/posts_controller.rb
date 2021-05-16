8768c3eba1644c5f3688df34b36c254ed6059b58
angelo <aurtula1@gmail.com>
2021-05-16 09:12:27 -0400

    commit tree f86c975ad802fa5fe88f273ced922f852b995890

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,35 @@
 class PostsController < ApplicationController
-  before_action :set_post!, only: [:show, :edit, :update]
+  before_action :set_post!, only: [:show, :edit, :update, :new, :create]
 
   def show
+    set_post!
   end
 
   def edit
+    set_post!
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def update
+    set_post!
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
